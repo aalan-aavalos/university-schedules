9790871a74f92357d2e3f92c1e476eb17313f7a1
aalan-aavalos <avalosalan789@gmail.com>
2024-10-19 23:21:27 -0600

    project configuration ready

---
+++ src/app/provider.tsx
@@ -0,0 +1,53 @@
+"use client"; // Asegura que este componente se ejecute en el navegador
+
+import React from "react";
+
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+
+import { ConfirmProvider } from "material-ui-confirm";
+import { SnackbarProvider } from "notistack";
+
+// Configuración de Amplify
+import { Amplify } from "aws-amplify";
+import amplifyConfig from "../aws-exports";
+
+Amplify.configure(amplifyConfig);
+
+import { ButtonProps } from "@mui/material";
+import { ConfirmOptions } from "material-ui-confirm";
+
+interface CustomConfirmOptions extends ConfirmOptions {
+  confirmationButtonProps?: ButtonProps;
+  cancellationButtonProps?: ButtonProps;
+}
+
+const optionsConfirmProvider: CustomConfirmOptions = {
+  confirmationButtonProps: { variant: "contained", "aria-hidden": false },
+  confirmationText: "Aceptar",
+  cancellationButtonProps: {
+    variant: "contained",
+    color: "error",
+    "aria-hidden": false,
+  },
+  cancellationText: "Cancelar",
+  allowClose: false,
+};
+
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ConfirmProvider defaultOptions={optionsConfirmProvider}>
+      <SnackbarProvider
+        maxSnack={2}
+        autoHideDuration={2500}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "center",
+        }}
+      >
+        <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
+      </SnackbarProvider>
+    </ConfirmProvider>
+  );
+};
+
+export { Provider };
--- src/components/ButtonQuery.tsx
+++ src/components/ButtonQuery.tsx
@@ -5,21 +5,40 @@ import React from "react";
 import { generateClient } from "aws-amplify/api";
 import { listTodos } from "@/graphql/queries";
 
+import { Button } from "@mui/material";
+
+import { enqueueSnackbar } from "notistack";
+import { useConfirm } from "material-ui-confirm";
+
 const client = generateClient();
 
 const ButtonQuery = () => {
-  const query_data = async (name: string = "Invitado") => {
-    const allTodos = await client.graphql({
-      query: listTodos,
-    });
+  const confirm = useConfirm();
 
-    console.log("Bienvenido,", name);
-    console.log("consultando....");
-    console.log(allTodos);
+  const query_data = async (name: string = "Invitado") => {
+    confirm({ description: "This action is permanent!" })
+      .then(async () => {
+        const allTodos = await client.graphql({
+          query: listTodos,
+        });
+
+        console.log("Bienvenido,", name);
+        console.log("consultando....");
+        console.log(allTodos);
+
+        enqueueSnackbar("That was easy!");
+      })
+      .catch(() => {
+        enqueueSnackbar("That wasn't easy!");
+      });
   };
   return (
     <>
-      <button onClick={() => query_data()}>Consultar datos</button>
+      <Button variant="contained" onClick={() => query_data()}>
+        Consultar datos
+      </Button>
+
+      <div className="p-2 bg-blue-500 ">Hola</div>
     </>
   );
 };
