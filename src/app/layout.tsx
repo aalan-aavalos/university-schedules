56619263d1c1a636c9ebb72d3dad5ccdd9816f79
aalan-aavalos <avalosalan789@gmail.com>
2024-10-22 22:34:11 -0600

    build ready

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
+import Padoru from "@/public/gifs/padoru.gif";
+
 import { Provider } from "./provider";
 
 export const metadata: Metadata = {
   title: "University Schedules",
   description: "Aplicación para crear horarios para estudiantes",
+  icons: { icon: Padoru.src },
 };
 
 export default function RootLayout({
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { fetchUserAttributes, FetchUserAttributesOutput } from 'aws-amplify/auth';
+import { useRouter } from 'next/navigation';
 
 interface UseAuthResult {
     isAuthenticated: boolean;
@@ -7,28 +8,31 @@ interface UseAuthResult {
     loading: boolean;
 }
 
-function useAuth(): UseAuthResult {
+function useAuth(allowedRoles?: string[]): UseAuthResult {
     const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
     const [user, setUser] = useState<FetchUserAttributesOutput | null>(null);
     const [loading, setLoading] = useState<boolean>(true);
+    const router = useRouter();
 
     useEffect(() => {
         async function checkAuthStatus() {
             try {
                 const currentUser = await fetchUserAttributes();
+
                 setIsAuthenticated(true);
                 setUser(currentUser);
             } catch (err) {
                 setIsAuthenticated(false);
                 setUser(null);
 
-                console.error(err)
+                router.push('/');
+                console.error(err);
             } finally {
                 setLoading(false);
             }
         }
         checkAuthStatus();
-    }, []);
+    }, [allowedRoles, router]);
 
     return { isAuthenticated, user, loading };
 }
